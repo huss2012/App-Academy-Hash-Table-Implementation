ab732b0d1e0faf315cdb494ce9201e9e14694d02
huss2012 <huss24940@gmail.com>
2024-05-27 01:40:07 +0300

    read instance method is done.

--- phases/01-implementation.js
+++ phases/01-implementation.js
@@ -34,11 +34,55 @@ class HashTable { // get O(1), set O(1), deleteKey O(1)
 
   insert(key, value) {
     // Your code here
+    //1.Create the keyValuePair:
+    let keyValuePair = new KeyValuePair(key, value);
+    //2.Create the index:
+    let index = this.hashMod(key);
+    //Check if there is an element at the index:
+    if (this.data[index]) {
+      //Handel same key different value insertion:
+      /*The goal is to search in the linked list for the same key if so overwrite the value
+      ToDo so we need to loop through the linked list using while loop till reach to null pointer*/
+      let head = this.data[index];
+
+      while (head) {
+        if (head.key === key) {
+          head.value = value;
+          return;
+        }
+        //update the head => move to the next node:
+        head = head.next;
+      }
+
+      //Handel different key same hash insertion:
+      keyValuePair.next = this.data[index];//add to the head of the linkled list.
+    }
+    //3.Insert at the index:
+    this.data[index] = keyValuePair;//Update the head of the likned list to point to the current insearted keyValuePair.
+    //4.Update the count:
+    this.count++;
   }
 
 
   read(key) {
     // Your code here
+    let index = this.hashMod(key);
+    let head = this.data[index];
+
+    /*mean there is more then one element at the index
+    => there is a linked list, a hash collison happend*/
+    if (head.next) {
+      while (head) {
+        if (head.key === key) {
+          return head.value;
+        }
+        //Update the head or move through the linked list.
+        head = head.next;
+      }
+    }
+    if (head.key !== key) return undefined;
+    //if there is an element
+    return this.data[index].value;//this will work if there is not hash collison inseartion
   }
 
 
